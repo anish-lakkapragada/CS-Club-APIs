97d423d689b21d8452ffd085bc644096cfd30df2
anish-lkakapragada <anish.lakkapragada@gmail.com>
2022-03-28 18:17:05 -0700

    phew

--- api.py
+++ api.py
@@ -1,17 +1,24 @@
 from flask import Flask, request 
-import requests
+import requests 
 
 app = Flask(__name__)
 
 @app.route("/")
 def default(): 
-    return "Hello APIs"
+    return "hello APIs"
 
-@app.route("/pageid")
-def pageid(): 
-    page = request.args.get("page")
-    response = requests.get("http://en.wikipedia.org/w/api.php", {"format": "json", "action": "query", "titles": page})
-    json = response.json()
-    return json
+@app.route("/resource1")
+def resource1(): 
+    name = request.args.get("name")
+    return f"hello {name}"
+
+@app.route("/wikipediaWrap")
+def wrap(): 
+    titles = request.args.get("title")
+    response = requests.get("http://en.wikipedia.org/w/api.php", {"action" : "query", "titles": titles, "format": "json"})
+    if (response.status_code != 200): 
+        return "bozo"
+    return response.json()
+
 
 app.run(debug=True)
